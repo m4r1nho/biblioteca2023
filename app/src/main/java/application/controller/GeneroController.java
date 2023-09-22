06b180e1126ced48566b2d074c82a33386646013
Guilherme S. Marinho <guilherme.sm20032016@gmail.com>
2023-09-22 01:42:51 +0000

    salvar21d

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -2,45 +2,40 @@ package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import application.model.Genero;
 import application.model.GeneroRepository;
 
-@controller
+@Controller
 @RequestMapping("/genero")
-public class GeneroController{
+public class GeneroController {
     @Autowired
     private GeneroRepository generoRepo;
 
     @RequestMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
         model.addAttribute("generos", generoRepo.findAll());
-        return "/genero/list";
+        return "genero/list";
     }
 
     @RequestMapping("/insert")
-    public String insert(){
-        return "/genero/insert";
-
+    public String insert() {
+        return "genero/insert";
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    public String insert(@RequestParam("nome") String nome){
+    public ModelAndView insert(@RequestParam("nome") String nome) {
         Genero genero = new Genero();
         genero.setNome(nome);
 
         generoRepo.save(genero);
 
-        return "redirect:/genero/list";
-        
-
-
+        ModelAndView modelAndView = new ModelAndView("redirect:/genero/list");
+        return modelAndView;
     }
-
-
-
-
 }

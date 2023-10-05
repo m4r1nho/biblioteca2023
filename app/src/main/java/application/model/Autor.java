06b1188f182ee3135322777526edbe217d7b8afa
Guilherme S. Marinho <guilherme.sm20032016@gmail.com>
2023-10-05 23:05:51 +0000

    salvar

---
+++ app/src/main/java/application/model/Autor.java
@@ -0,0 +1,32 @@
+package application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome; // Correção: atribuir o valor do parâmetro nome ao campo nome
+    }
+}
